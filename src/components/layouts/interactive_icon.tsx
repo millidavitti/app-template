6a6ef30e3d274a0c6427ba78e91e7d73d9fb9585
davitti <abuadonald@gmail.com>
2025-03-21 11:33:19 +0100

    feat(ui): enhance layout components & improve interactivity
    - Added `debug` prop to `Flex` for easier layout debugging
    - Introduced `htmlProps` support in `InteractiveIcon` and `Button` for better extensibility
    - Refactored `Overlay` for dynamic state-based visibility and improved click handling
    - Updated styling for better consistency across components

--- src/components/layouts/interactive_icon.tsx
+++ src/components/layouts/interactive_icon.tsx
@@ -1,23 +1,26 @@
-import { MouseEventHandler, ReactNode } from "react";
+import { HTMLProps, MouseEventHandler, ReactNode } from "react";
 import { cn } from "@/utils/cn";
 
 interface InteractiveIcon {
 	children: ReactNode;
 	className?: string;
 	callback?: MouseEventHandler<HTMLDivElement> | undefined;
+	htmlProps?: Partial<HTMLProps<HTMLDivElement>>;
 }
 export default function InteractiveIcon({
 	children,
 	className,
 	callback,
+	htmlProps,
 }: InteractiveIcon) {
 	return (
 		<div
 			className={cn(
-				"p-3 cursor-pointer active:scale-95 transition stroke-light-surface-on-surface",
+				"p-1 cursor-pointer active:scale-95 transition stroke-light-surface-on-surface shrink-0",
 				className,
 			)}
 			onClick={callback}
+			{...htmlProps}
 		>
 			{children}
 		</div>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,17 +1,25 @@
 import { cn } from "@/utils/cn";
-import React, { MouseEventHandler, ReactNode } from "react";
+import React, { HTMLProps, MouseEventHandler, ReactNode } from "react";
 interface Button {
 	children: ReactNode;
 	className?: string;
 	onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+	htmlProps?: Partial<HTMLProps<HTMLButtonElement>>;
 	type?: "button" | "submit";
 }
-export default function Button({ children, className, onClick, type }: Button) {
+export default function Button({
+	children,
+	className,
+	onClick,
+	type = "button",
+	htmlProps,
+}: Button) {
 	return (
 		<button
+			{...htmlProps}
 			type={type}
 			className={cn(
-				"outline p-3 py-2 title-medium active:scale-95 transition",
+				"flex justify-center gap-3 border border-border p-3 py-2 title-medium active:scale-[.99] transition shrink-0",
 				className,
 			)}
 			onClick={onClick}
